29cae5081dc4dbf96ca08fe015f3324971c91dee
Lisandro <lisandroandia14@gmail.com>
2026-03-03 18:15:03 -0300

    deploy merge fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-import { redirect } from "next/navigation";
+import { Navbar } from "@/components/layout/Navbar";
+import { Footer } from "@/components/layout/Footer";
+import { HeroSection } from "@/components/sections/HeroSection";
+import { CombosSection } from "@/components/sections/CombosSection";
+import { AboutSection } from "@/components/sections/AboutSection";
+import { EventsSection } from "@/components/sections/EventsSection";
+import { ContactSection } from "@/components/sections/ContactSection";
 
 export default function Home() {
   return (
